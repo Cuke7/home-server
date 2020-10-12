58f94b0d20e7829c475db77f210f8eaed456202a
Cuke7 <cassany.louis@gmail.com>
2020-10-12 17:22:23 +0200

    Initial commit

---
+++ tram_data.js
@@ -0,0 +1,360 @@
+let bordeaux = [
+    [
+        "Saige",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3725",
+    ],
+    [
+        "Unitec",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3727",
+    ],
+    [
+        "Montaigne - Montesquieu",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3729",
+    ],
+    [
+        "Doyen Brus",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3733",
+    ],
+    [
+        "François Bordes",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3735",
+    ],
+    [
+        "Arts & Métiers",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3737",
+    ],
+    [
+        "Béthanie",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3739",
+    ],
+    [
+        "Peixotto",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3741",
+    ],
+    [
+        "Forum",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3743",
+    ],
+    [
+        "Roustaing",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3745",
+    ],
+    [
+        "Barrière Saint-Genès",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3747",
+    ],
+    [
+        "Bergonié",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3749",
+    ],
+    [
+        "Saint-Nicolas",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3751",
+    ],
+    [
+        "Victoire",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3754",
+    ],
+    [
+        "Musée d'Aquitaine",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3755",
+    ],
+    [
+        "Hôtel de Ville",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3757",
+    ],
+    [
+        "Gambetta - madd",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3759",
+    ],
+    [
+        "Grand Théâtre",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3761",
+    ],
+    [
+        "Quinconces",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A3763",
+    ],
+    [
+        "Bougnard",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5244",
+    ],
+    [
+        "Camponac Médiathèque",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5245",
+    ],
+    [
+        "Pessac Centre",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5247",
+    ],
+    [
+        "CAPC Musée d'Art Contemporain",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5253",
+    ],
+    [
+        "Chartrons",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5255",
+    ],
+    [
+        "Cours du Médoc",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5257",
+    ],
+    [
+        "Les Hangars",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5259",
+    ],
+    [
+        "La Cité du Vin",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5261",
+    ],
+    [
+        "Rue Achard",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5528",
+    ],
+    [
+        "New York",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5530",
+    ],
+    [
+        "Brandenburg",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5532",
+    ],
+    [
+        "Claveau",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A5534",
+    ],
+    [
+        "Berges de la Garonne",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A7172",
+    ],
+    [
+        "Chataigneraie",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A7432",
+    ],
+    [
+        "Cap Métiers",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A7434",
+    ],
+    [
+        "Hôpital Haut-Lévêque",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A7436",
+    ],
+    [
+        "Gare Pessac Alouette",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A7438",
+    ],
+    [
+        "France Alouette",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60/stop_point%3ATBT%3ASP%3A7440",
+    ],
+];
+
+let pessac = [
+    [
+        "Bougnard",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3724",
+    ],
+    [
+        "Saige",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3726",
+    ],
+    [
+        "Unitec",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3728",
+    ],
+    [
+        "Montaigne - Montesquieu",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3730",
+    ],
+    [
+        "Doyen Brus",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3734",
+    ],
+    [
+        "François Bordes",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3736",
+    ],
+    [
+        "Arts & Métiers",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3738",
+    ],
+    [
+        "Béthanie",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3740",
+    ],
+    [
+        "Peixotto",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3742",
+    ],
+    [
+        "Forum",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3744",
+    ],
+    [
+        "Roustaing",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3746",
+    ],
+    [
+        "Barrière Saint-Genès",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3748",
+    ],
+    [
+        "Bergonié",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3750",
+    ],
+    [
+        "Saint-Nicolas",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3752",
+    ],
+    [
+        "Victoire",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3753",
+    ],
+    [
+        "Musée d'Aquitaine",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3756",
+    ],
+    [
+        "Hôtel de Ville",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3758",
+    ],
+    [
+        "Gambetta - madd",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3760",
+    ],
+    [
+        "Grand Théâtre",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3762",
+    ],
+    [
+        "Quinconces",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A3764",
+    ],
+    [
+        "Camponac Médiathèque",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5246",
+    ],
+    [
+        "Pessac Centre",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5247",
+    ],
+    [
+        "CAPC Musée d'Art Contemporain",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5252",
+    ],
+    [
+        "Chartrons",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5254",
+    ],
+    [
+        "Cours du Médoc",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5256",
+    ],
+    [
+        "Les Hangars",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5258",
+    ],
+    [
+        "La Cité du Vin",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5260",
+    ],
+    [
+        "Rue Achard",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5529",
+    ],
+    [
+        "New York",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5531",
+    ],
+    [
+        "Brandenburg",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5533",
+    ],
+    [
+        "Claveau",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A5535",
+    ],
+    [
+        "Berges de la Garonne",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A7172",
+    ],
+    [
+        "Chataigneraie",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A7431",
+    ],
+    [
+        "Cap Métiers",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A7433",
+    ],
+    [
+        "Hôpital Haut-Lévêque",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A7435",
+    ],
+    [
+        "Gare Pessac Alouette",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A7437",
+    ],
+    [
+        "France Alouette",
+        " https://www.infotbm.com/fr/horaires/detail/60/route%3ATBT%3A60_R/stop_point%3ATBT%3ASP%3A7440",
+    ],
+];
+
+let dictionary = [
+    "Saige",
+    "Unitec",
+    "Montaigne - Montesquieu",
+    "Doyen Brus",
+    "François Bordes",
+    "Arts & Métiers",
+    "Béthanie",
+    "Peixotto",
+    "Forum",
+    "Roustaing",
+    "Barrière Saint-Genès",
+    "Bergonié",
+    "Saint-Nicolas",
+    "Victoire",
+    "Musée d'Aquitaine",
+    "Hôtel de Ville",
+    "Gambetta - madd",
+    "Grand Théâtre",
+    "Quinconces",
+    "Camponac Médiathèque",
+    "Pessac Centre",
+    "CAPC Musée d'Art Contemporain",
+    "Chartrons",
+    "Cours du Médoc",
+    "Les Hangars",
+    "La Cité du Vin",
+    "rue Achard",
+    "New York",
+    "Brandenburg",
+    "Claveau",
+    "Berges de la Garonne",
+    "Quinconces Fleuve",
+    "Chataigneraie",
+    "Cap Métiers",
+    "Hôpital Haut-Lévêque",
+    "Gare Pessac Alouette",
+    "France Alouette",
+    "Bougnard",
+];
+
+let geoloc = [
+    { name: "Roustaing", pos: [44.816894, -0.585708] },
+    { name: "Forum", pos: [44.812086, -0.590992] },
+    { name: "Victoire", pos: [44.831291, -0.573222] },
+    { name: "Hôtel de ville", pos: [44.837682, -0.576379] },
+    { name: "Doyen Brus", pos: [44.800227, -0.609602] },
+    { name: "Peixotto", pos: [44.807239, -0.592637] },
+    { name: "Grand théâtre", pos: [44.842013, -0.575216] },
+    { name: "Gambetta", pos: [44.840066, -0.578275] },
+];
+
+module.exports = {
+    bordeaux: bordeaux,
+    pessac: pessac,
+    dictionary: dictionary,
+    geoloc: geoloc,
+};
